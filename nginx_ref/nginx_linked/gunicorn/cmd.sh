f8ec988b41fcee51d7b296ad941d3bc72b34dca3
takagotch <dyaccb@gmail.com>
2017-11-18 04:02:30 +0900

    gunicorn

---
+++ nginx_ref/nginx_linked/gunicorn/cmd.sh
@@ -0,0 +1,23 @@
+yum groupinstall "Development Tools"
+yum install epel-release
+yum install python-pip python-devel
+
+yum install postgresql-server postgresql-devel
+sudo -u postgresql initdb -D /var/lib/pgsql/data
+systemctl start postgresql
+createdb -U postgres bootcamp
+
+cd /opt
+git clone https://github.com/vitorfs/bootcamp.git
+cd /opt/bootcamp
+pip install -r requirements.pip
+
+cd /opt/bootcamp
+cat <FIN>.env
+
+cd /opt/bootcamp
+gunicorn --daemon bootcamp.wsgi:application
+
+sytemctl start nginx
+
+
