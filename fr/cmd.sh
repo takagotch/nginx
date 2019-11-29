46cdd3a9227a8b93f47bd5fd93dfaebd6365ed04
takagotch <dyaccb@gmail.com>
2019-11-30 01:36:02 +0900

    cat /usr/share/nginx/html/index.html

---
+++ fr/cmd.sh
@@ -0,0 +1,20 @@
+sudo yum localinstall -y http://nginx.org/packages/centos7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+sudo yum install -y nginx
+systemctl status nginx
+systemctl status nginx
+sudo systemctl start nginx
+sudo systemctl stop nginx
+sudo systemctl restart nginx
+systemctl is-enabled nginx
+sudo systemctl enable nginx
+systemctl is-enabled nginx
+curl http://192.168.33.10
+
+
+cat /etc/nginx/nginx.conf
+cat /etc/nginx/conf.d/default.conf
+cat /usr/share/nginx/html/index.html
+
+
+
+
